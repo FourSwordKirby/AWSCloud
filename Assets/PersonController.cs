9a332b2fce18d13b85202d865d2fd411012bf73c
fourswordkirby <rogliu26@gmail.com>
2016-09-18 00:40:23 -0400

    set up the basic framework for the chatting thing

---
+++ Assets/PersonController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PersonController : MonoBehaviour {
+
+    public DialogBox messageBox;
+    public InputField chatBox;
+
+    public List<KeyValuePair<string, string>> currentDialog;
+    public int dialogIndex;
+
+    public int currentState; //Use this to track the progress in the "story"
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (messageBox.dialogCompleted)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if(currentDialog != null && dialogIndex < currentDialog.Count)
+                {
+                    messageBox.displayDialog(currentDialog[dialogIndex].Key, currentDialog[dialogIndex].Value);
+                    dialogIndex++;
+                }
+                else
+                {
+                    currentDialog = null;
+                    dialogIndex = 0;
+                    chatBox.interactable = true;
+                }
+            }
+        }
+	}
+
+    public void InterpretResponse() {
+        string response = chatBox.text;
+        Debug.Log(response);
+        chatBox.text = "";
+        chatBox.interactable = false;
+
+        //Insert Logic for progressing the "story"
+        //Replace the text file path with whatever is appropriate
+        currentDialog = getDialogFromFile("C:/Users/Roger Liu/Desktop/test.txt");
+        messageBox.displayDialog(currentDialog[dialogIndex].Key, currentDialog[dialogIndex].Value);
+        dialogIndex++;
+    }
+
+    public List<KeyValuePair<string, string>> getDialogFromFile(string filename)
+    {
+        List<KeyValuePair<string, string>> dialog = new List<KeyValuePair<string, string>>();
+        System.IO.StreamReader file = new System.IO.StreamReader(filename);
+
+        string line = "";
+        while ((line = file.ReadLine()) != null)
+        {
+            string speaker = line.Split('=')[0];
+            string words = line.Split('=')[1];
+            dialog.Add(new KeyValuePair<string, string>(speaker, words));
+        }
+        return dialog;
+    }
+}
---
+++ Assets/Scripts/DialogUI.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+
+public class DialogUI : MonoBehaviour
+{
+    public DialogBox dialogBox;
+
+    public DialogBox SpeakerImage;
+    
+    private float displayDelay = 0.5f;
+
+    public void Update()
+    {
+        if (!dialogCompleted())
+        {
+            displayDelay = 0.5f;
+        }
+        else if (dialogBox.dialogField.text != "")
+        {
+            displayDelay -= Time.deltaTime;
+        }
+    }
+    
+
+	public void clearLoveInterests() {
+	}
+
+    public bool dialogCompleted()
+    {
+        return dialogBox.dialogCompleted;
+    }
+
+    public void displayNormal()
+    {
+        dialogBox.dialogField.fontStyle = FontStyle.Normal;
+    }
+
+    public void displayDialog(string name, string text, DisplaySpeed speed = DisplaySpeed.fast)
+    {
+        dialogBox.displayDialog(name, text, speed);
+    }
+
+    public void resolveDialog()
+    {
+        dialogBox.resolveDialog();
+    }
+
+    public void clearDialogBox()
+    {
+        dialogBox.displayDialog("", "");
+        dialogBox.dialogCompleted = true;
+    }
+
+	public void closeDialogBox() {
+		dialogBox.closeDialog ();
+	}
+}
