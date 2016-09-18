190c1824c14a9526aad03580c134af0eb97837cf
Ralph <ralph@puppyrush.net>
2016-09-18 17:54:49 -0400

    NLP added

---
+++ Assets/Scripts/NLPInterface.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using IBM.Watson.DeveloperCloud.Services.AlchemyAPI.v1;
+using IBM.Watson.DeveloperCloud.Logging;
+
+public class NLPInterface : MonoBehaviour {
+	static AlchemyAPI m_AlchemyLanguage = new AlchemyAPI();
+	public bool isInterpreting {get; private set;}
+	public string curSentiment {get; private set;}
+
+	public void GetTextSentiment(string input)
+	{
+		isInterpreting = true;
+		if(!m_AlchemyLanguage.GetTextSentiment(OnGetTextSentiment, input))
+			print("ExampleAlchemyLanguage "+"Failed to get sentiment by text POST");
+	}
+
+	private void OnGetTextSentiment(SentimentData sentimentData, string data)
+	{
+		if(sentimentData != null)
+		{
+			/*
+			print("ExampleAlchemyLanguage "+"status: {0} "+sentimentData.status);
+			print("ExampleAlchemyLanguage "+"url: {0} "+sentimentData.url);
+			print("ExampleAlchemyLanguage "+"language: {0} "+sentimentData.language);
+			print("ExampleAlchemyLanguage "+"text: {0} "+sentimentData.text);*/
+			if(sentimentData.docSentiment == null)
+				print("ExampleAlchemyLanguage "+"No sentiment found!");
+			else
+				if(sentimentData.docSentiment != null && !string.IsNullOrEmpty(sentimentData.docSentiment.type))
+					print("ExampleAlchemyLanguage "+"Sentiment: {0}, Score: {1} "+sentimentData.docSentiment.type+" "+sentimentData.docSentiment.score);
+			curSentiment = sentimentData.docSentiment.type;
+		}
+		else
+		{
+			print("ExampleAlchemyLanguage "+"Failed to find Relations!");
+		}
+		isInterpreting = false;
+	}
+}
---
+++ Assets/Scripts/PersonController.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.IO.Compression;
+
+public class PersonController : MonoBehaviour {
+
+    public DialogBox messageBox;
+    public InputField chatBox;
+	NLPInterface nlp;
+
+    public List<KeyValuePair<string, string>> currentDialog;
+    public int dialogIndex;
+
+    public int currentState; //Use this to track the progress in the "story"
+
+	// Use this for initialization
+	void Start () {
+		nlp = GetComponent<NLPInterface>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (messageBox.dialogCompleted)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if(currentDialog != null && dialogIndex < currentDialog.Count)
+                {
+                    messageBox.displayDialog(currentDialog[dialogIndex].Key, currentDialog[dialogIndex].Value);
+                    dialogIndex++;
+                }
+                else
+                {
+                    currentDialog = null;
+                    dialogIndex = 0;
+                    chatBox.interactable = true;
+                }
+            }
+        }
+	}
+
+    public void InterpretResponse() {
+		StartCoroutine(DoInterpretResponse());
+    }
+
+	IEnumerator DoInterpretResponse() {
+		string response = chatBox.text;
+//		Debug.Log(response);
+		chatBox.text = "";
+		chatBox.interactable = false;
+
+		//map response to emotion
+		nlp.GetTextSentiment(response);
+		while (nlp.isInterpreting) yield return new WaitForEndOfFrame();
+		print(nlp.curSentiment);
+
+		//Insert Logic for progressing the "story"
+
+		currentDialog = getDialogFromFile(Application.streamingAssetsPath + "/test.txt");
+		messageBox.displayDialog(currentDialog[dialogIndex].Key, currentDialog[dialogIndex].Value);
+		dialogIndex++;
+	}
+
+    public List<KeyValuePair<string, string>> getDialogFromFile(string filename)
+    {
+        List<KeyValuePair<string, string>> dialog = new List<KeyValuePair<string, string>>();
+        System.IO.StreamReader file = new System.IO.StreamReader(filename);
+
+        string line = "";
+        while ((line = file.ReadLine()) != null)
+        {
+            string speaker = line.Split('=')[0];
+            string words = line.Split('=')[1];
+            dialog.Add(new KeyValuePair<string, string>(speaker, words));
+        }
+        return dialog;
+    }
+}
